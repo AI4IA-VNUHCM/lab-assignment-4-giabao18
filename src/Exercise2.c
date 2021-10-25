0b6ef7ec040b080ee8c66b2e9fba4300c37341bf
giabao18 <phamgiabao1862001@gmail.com>
2021-10-25 15:10:30 +0700

    ex2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -28,9 +28,11 @@ void Array2Dconverter(int arr[], int a[SIZE][SIZE], int m, int n)
 {
 	int row, column;
 	int counter = 0;
-	//Convert 1D array to 2D array
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
+	// Convert 1D array to 2D array
+	for (row = 0; row <= (m - 1); row++)
+	{
+		for (column = 0; column <= (n - 1); column++)
+		{
 			a[row][column] = arr[counter];
 			counter++;
 		}
@@ -41,33 +43,71 @@ void printArray(int a[SIZE][SIZE], int m, int n)
 {
 	int row, column;
 
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
+	for (row = 0; row <= (m - 1); row++)
+	{
+		for (column = 0; column <= (n - 1); column++)
+		{
 			printf("%d ", a[row][column]);
 		}
 		printf("\n");
 	}
 }
 
-void Ex2(int arr[], int m, int n){
+void Ex2(int arr[], int m, int n)
+{
 	int a[SIZE][SIZE];
-	Array2Dconverter(arr,a,m,n);
-	//Your codes here
+	Array2Dconverter(arr, a, m, n);
+	// Your codes here
+	for (int i = 0; i < n; i++)
+	{
+		if (!(i & 1))
+		{ // even decrease
+			int k = 1;
+			while (k <= m - 1)
+			{
+				int x = a[k][i];
+				int vt = k - 1;
+				for (; vt >= 0 && a[vt][i] < x; vt--)
+				{
+					a[vt + 1][i] = a[vt][i];
+				}
+				a[vt + 1][i] = x;
+				k++;
+			}
+		}
+		else
+		{ // odd increse
+			int k = 1;
+			while (k <= m - 1)
+			{
+				int x = a[k][i];
+				int vt = k - 1;
+				for (; vt >= 0 && a[vt][i] > x; vt--)
+				{
+					a[vt + 1][i] = a[vt][i];
+				}
+				a[vt + 1][i] = x;
+				k++;
+			}
+		}
+	}
 
 	printArray(a, m, n);
 }
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
+int main(int argc, char *argv[])
+{
+	// testing variable, applying it to your algorithm for auto-evaluating
 	int row = atoi(argv[1]);
 	int col = atoi(argv[2]);
-	argc-=3;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		testcase[i] = atoi(argv[i+3]);
+	argc -= 3;
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++)
+	{
+		testcase[i] = atoi(argv[i + 3]);
 	}
 
 	Ex2(testcase, row, col);
-	
+
 	return 0;
 }
