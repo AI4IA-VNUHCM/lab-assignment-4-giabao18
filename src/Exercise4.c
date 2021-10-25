a2c0fd41d4b17cd68144cb7ad572373fc5533688
giabao18 <phamgiabao1862001@gmail.com>
2021-10-25 21:22:13 +0700

    EX4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -58,7 +58,7 @@ void insertRow(int arr[], int a[SIZE][SIZE], int rowIndex, int m, int n)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			a[j][i + 1] = a[j][i];
+			a[i + 1][j] = a[i][j];
 		}
 	}
 	for (int i = 0; i < n; i++)
